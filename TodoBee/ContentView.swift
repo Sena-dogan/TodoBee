c5287cfb13b6d1fbbcaf6f1387c0bea65c1889ae
Zeynep Sena Doğan <93337596+Sena-dogan@users.noreply.github.com>
2024-06-25 16:42:21 +0300

    Initial Commit

---
+++ TodoBee/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  TodoBee
+//
+//  Created by Zeynep Sena Doğan on 25.06.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TodoBee/TodoBeeApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoBeeApp.swift
+//  TodoBee
+//
+//  Created by Zeynep Sena Doğan on 25.06.2024.
+//
+
+import SwiftUI
+
+@main
+struct TodoBeeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
